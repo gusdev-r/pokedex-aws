d04fbf7f1f4c58b1d7834d360a8b9af5f39a88eb
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-04-17 17:51:36 -0300

    feat(pokemon): adding service and controller

---
+++ src/main/java/com/academy/gusdev/pokedex/controller/PokemonController.java
@@ -0,0 +1,2 @@
+package com.academy.gusdev.pokedex.controller;public class PokemonController {
+}
---
+++ src/main/java/com/academy/gusdev/pokedex/service/PokemonService.java
@@ -0,0 +1,40 @@
+package com.academy.gusdev.pokedex.service;
+
+import com.academy.gusdev.pokedex.domain.Pokemon;
+import com.academy.gusdev.pokedex.repository.PokemonRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class PokemonService {
+
+    private PokemonRepository pokemonRepository;
+
+    public PokemonService() {
+    }
+
+    public PokemonService(PokemonRepository pokemonRepository) {
+        this.pokemonRepository = pokemonRepository;
+    }
+
+    public Flux<Pokemon> getAllPokemon() {
+        return pokemonRepository.findAll();
+    }
+    public Mono<Pokemon> getPokemonById(String id) {
+        return pokemonRepository.findById(id);
+    }
+    public Mono<Pokemon> savePokemon(Pokemon pokemon) {
+        return pokemonRepository.save(pokemon);
+    }
+    public Mono<Pokemon> updatePokemon(String id, Pokemon pokemon) {
+        return pokemonRepository.findById(id)
+                .flatMap(pokemonFound -> {
+                    pokemonFound.setName(pokemon.getName());
+                    pokemonFound.setName(pokemon.getCategory());
+                    pokemonFound.setName(pokemon.getSkills());
+                    pokemonFound.setHealth(pokemon.getHealth());
+                    return pokemonRepository.save(pokemonFound);
+                });
+    }
+}
