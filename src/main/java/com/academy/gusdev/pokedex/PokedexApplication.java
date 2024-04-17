41bd0bec94dfc070751df459af601d0dba587e1f
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-04-17 20:04:05 -0300

    feat: add fixed objects

--- src/main/java/com/academy/gusdev/pokedex/PokedexApplication.java
+++ src/main/java/com/academy/gusdev/pokedex/PokedexApplication.java
@@ -1,7 +1,13 @@
 package com.academy.gusdev.pokedex;
 
+import com.academy.gusdev.pokedex.domain.Pokemon;
+import com.academy.gusdev.pokedex.repository.PokemonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class PokedexApplication {
@@ -10,4 +16,19 @@ public class PokedexApplication {
 		SpringApplication.run(PokedexApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init (ReactiveMongoOperations operations, PokemonRepository repository) {
+		return args -> {
+			Flux<Pokemon> pokemonFlux = Flux.just(
+							new Pokemon(null, "Bulbassauro", "Semente", "OverGrow", 6.09),
+							new Pokemon(null, "Charizard", "Fogo", "Blaze", 90.05),
+							new Pokemon(null, "Caterpie", "Minhoca", "Poeira do Escudo", 2.09),
+							new Pokemon(null, "Blastoise", "Marisco", "Torrente", 6.09))
+					.flatMap(repository::save);
+
+			pokemonFlux
+					.thenMany(repository.findAll())
+					.subscribe(System.out::println);
+		};
+	}
 }
